81c72b2bddaaa9fc8f0f47a2302bc39072631ce4
Tyson Williams <tyson.j.williams@gmail.com>
2018-11-14 13:53:32 -0700

    Create basic Layout component
    Create component
    Wrap component in a hoc
    Add component to App with a test paragraph
    Test: everything showing up in browser

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import Layout from "./components/Layout/Layout.js";
 
 class App extends Component {
   render() {
     return (
       <div>
-
+        <Layout>
+          <p>test</p>
+        </Layout>
       </div>
     );
   }
