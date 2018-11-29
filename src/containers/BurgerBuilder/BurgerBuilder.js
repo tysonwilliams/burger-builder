1e61aff9fcaf00c9a3c5a6bab77d97035081d73c
Tyson Williams <tyson.j.williams@gmail.com>
2018-11-29 15:57:29 -0700

    Create and add Burger component to BurgerBuilder

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,12 +1,14 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+
 import Aux from '../../hoc/Aux.js';
+import Burger from "../../components/Burger/Burger.js";
 
 class BurgerBuilder extends Component {
 
   render () {
     return (
       <Aux>
-        <div>Burger</div>
+        <Burger />
         <div>Build Controls</div>
       </Aux>
     );
