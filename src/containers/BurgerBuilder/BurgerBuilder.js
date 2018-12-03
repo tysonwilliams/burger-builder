18e2e55c399fdc8fd372fdbbc6c5af73b7032ad9
Tyson Williams <tyson.j.williams@gmail.com>
2018-12-03 13:28:17 -0700

    Add pricing to BurgerBuilder

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -4,6 +4,13 @@ import Aux from '../../hoc/Aux.js';
 import Burger from "../../components/Burger/Burger.js";
 import BuildControls from "../../components/Burger/BuildControls/BuildControls.js";
 
+const INGREDIENT_PRICES = {
+  bacon: 0.5,
+  salad: 0.4,
+  cheese: 1.2,
+  meat: 1.4,
+};
+
 class BurgerBuilder extends Component {
   state = {
     ingredients: {
@@ -11,7 +18,8 @@ class BurgerBuilder extends Component {
       salad: 0,
       cheese: 0,
       meat: 0,
-    }
+    },
+    totalPrice: 4,
   }
 
   render () {
