525ef1f37377612a56606edc951399cfe66fd1a6
Tyson Williams <tyson.j.williams@gmail.com>
2018-12-06 13:29:39 -0700

    Match build control order with burger ingredient placement

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -4,8 +4,8 @@ import classes from "./BuildControls.module.css";
 import BuildControl from "./BuildControl/BuildControl.js";
 
 const controls = [
-  { label: "Salad", type: "salad" },
   { label: "Bacon", type: "bacon" },
+  { label: "Salad", type: "salad" },
   { label: "Cheese", type: "cheese" },
   { label: "Meat", type: "meat" },
 ];
