535c3d1b6d6686f95fe316e2efc6d0d60b41bc5c
Tyson Williams <tyson.j.williams@gmail.com>
2018-12-27 18:47:58 -0700

    Create initial Toolbar or Header Menu

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,10 +1,12 @@
 import React from "react";
+
 import Aux from "../../hoc/Aux.js";
 import classes from "./Layout.module.css";
+import Toolbar from "../Navigation/Toolbar/Toolbar.js";
 
-const layout = (props) => (
+const layout = props => (
   <Aux>
-    <div>Toolbar, SideDrawer, Backdrop</div>
+    <Toolbar />
     <main className={classes.Content}>
       {props.children}
     </main>
